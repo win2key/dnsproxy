f8b71cfda70ccd662052a896719b583b85c446f5
Pixel <win2key@gmail.com>
2024-07-13 18:01:35 +0300

    bind DNS on start, release on exit + signaling

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -53,7 +60,55 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	w.WriteMsg(&msg)
 }
 
+func setDNSOnStart() {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("networksetup", "-setdnsservers", "Wi-Fi", "127.0.0.1")
+	case "linux":
+		cmd = exec.Command("nmcli", "device", "modify", "eth0", "ipv4.dns", "127.0.0.1")
+	case "windows":
+		cmd = exec.Command("powershell", "Set-DnsClientServerAddress", "-InterfaceAlias", "\"Wi-Fi\"", "-ServerAddresses", "127.0.0.1")
+	default:
+		log.Fatalf("Unsupported OS: %s", runtime.GOOS)
+	}
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Failed to set DNS on start: %v", err)
+	}
+	log.Printf("DNS set to 127.0.0.1 on start")
+}
+
+func resetDNSOnExit() {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("networksetup", "-setdnsservers", "Wi-Fi", "empty")
+	case "linux":
+		cmd = exec.Command("nmcli", "device", "modify", "eth0", "ipv4.ignore-auto-dns", "yes")
+	case "windows":
+		cmd = exec.Command("powershell", "Set-DnsClientServerAddress", "-InterfaceAlias", "\"Wi-Fi\"", "-ResetServerAddresses")
+	default:
+		log.Fatalf("Unsupported OS: %s", runtime.GOOS)
+	}
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Failed to reset DNS on exit: %v", err)
+	}
+	log.Printf("DNS reset to default on exit")
+}
+
 func main() {
+	// Set DNS on start
+	setDNSOnStart()
+
+	// Ensure DNS is reset on exit
+	defer resetDNSOnExit()
+
+	// Setup signal handling to catch interrupts and properly exit
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	// Setup the DNS server
 	dns.HandleFunc(".", handleDNSRequest)
 
@@ -63,23 +118,42 @@ func main() {
 		Net:  "udp",
 	}
 
-	go func() {
-		log.Printf("Starting UDP DNS server on %s", udpServer.Addr)
-		err := udpServer.ListenAndServe()
-		if err != nil {
-			log.Fatalf("Failed to start UDP DNS server: %s\n", err.Error())
-		}
-	}()
-
 	// Listen on TCP
 	tcpServer := &dns.Server{
 		Addr: "127.0.0.1:53",
 		Net:  "tcp",
 	}
 
-	log.Printf("Starting TCP DNS server on %s", tcpServer.Addr)
-	err := tcpServer.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Failed to start TCP DNS server: %s\n", err.Error())
+	go func() {
+		log.Printf("Starting UDP DNS server on %s", udpServer.Addr)
+		if err := udpServer.ListenAndServe(); err != nil {
+			log.Printf("Failed to start UDP DNS server: %s\n", err.Error())
+			sigs <- syscall.SIGTERM
+		}
+	}()
+
+	go func() {
+		log.Printf("Starting TCP DNS server on %s", tcpServer.Addr)
+		if err := tcpServer.ListenAndServe(); err != nil {
+			log.Printf("Failed to start TCP DNS server: %s\n", err.Error())
+			sigs <- syscall.SIGTERM
+		}
+	}()
+
+	sig := <-sigs
+	log.Printf("Received signal: %v", sig)
+
+	// Shutdown the DNS servers with a timeout context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := udpServer.ShutdownContext(ctx); err != nil {
+		log.Printf("Failed to gracefully shutdown UDP server: %v", err)
 	}
+
+	if err := tcpServer.ShutdownContext(ctx); err != nil {
+		log.Printf("Failed to gracefully shutdown TCP server: %v", err)
+	}
+
+	log.Println("Exiting program")
 }
